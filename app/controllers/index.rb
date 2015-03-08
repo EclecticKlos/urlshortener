10460a5b9ca47997463a78868c5f5863130f7323
EclecticKlos <dsklos@gmail.com>
2015-03-08 13:15:56 -0700

    Bare bones works, needs heavy visual uplift

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,16 @@
 get '/' do
   # Look in app/views/index.erb
+
+  @all_urls = Urls.all
+
   erb :index
+end
+
+post '/' do
+  u = Urls.new
+  u.full_url = params[:link]
+  u.short_url = "http://127.0.0.1:9393/" + Urls.last.id.to_s
+  u.save
+
+  redirect '/'
 end
